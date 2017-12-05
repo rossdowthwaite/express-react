a6c96f459b84bba51153b2273d0688b0e301e238
Ross Dowthwaite <ross.dowthwaite@theunit.co.uk>
2017-12-05 16:28:33 +0000

    connect front to back

--- react-backend/routes/users.js
+++ react-backend/routes/users.js
@@ -3,18 +3,14 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  // res.send('respond with a resource');
 
-  res.json([
-    {
-      id: 1,
-      username: 'rossdowthwaite'
-    },
-    {
-      id: 2,
-      username: 'pennylunn'
-    }
-  ])
+  res.json([{
+  	id: 1,
+  	username: "rossdowthwaite"
+  }, {
+  	id: 2,
+  	username: "pennylunn"
+  }]);
 
 });
 
